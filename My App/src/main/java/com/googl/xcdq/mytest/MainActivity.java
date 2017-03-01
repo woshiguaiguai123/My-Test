5202fde3e7f3f920e28f4fb77c326bc8450806d9
woshiguaiguai <1565228530@qq.com>
2017-03-01 23:28:37 +0800

    这是修改提交

--- My App/src/main/java/com/googl/xcdq/mytest/MainActivity.java
+++ My App/src/main/java/com/googl/xcdq/mytest/MainActivity.java
@@ -9,12 +9,18 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-private TextView textView;
+
+
+
+
+    
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView= (TextView) findViewById(R.id.textView);
+        textView = (TextView) findViewById(R.id.textView);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,17 +33,17 @@ private TextView textView;
 //                intent.setData(Uri.parse("tel:10086"));
 //                startActivity(intent);
 
-                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
-                startActivityForResult(intent,1);
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivityForResult(intent, 1);
             }
         });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode){
+        switch (requestCode) {
             case 1:
-                if (resultCode==RESULT_OK) {
+                if (resultCode == RESULT_OK) {
                     String s = data.getStringExtra("key");
                     Log.d("MainActivity", "s");
                 }
